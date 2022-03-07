7dbadb03a61fa8638800611d6bf785283c5adf9b
BystrovaV <79499241+BystrovaV@users.noreply.github.com>
2022-03-07 07:03:36 +0000

    Add function to main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,33 @@
 /* hello.c */
 #include <stdio.h>
+#include <string.h>
 
 void main (void)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  char letter;
+  fgets(&letter, 2, stdin);
+
+  char buff[2];
+  fgets(buff, 2, stdin);
+
+  char str[100];
+  int count = 0;
+  
+  while(1)
+  {
+    fgets(str, 100, stdin);
+    
+    if(strcmp(str, "exit\n") == 0)
+      break;
+      
+    for(int i=0; i < strlen(str);i++)
+    {
+      if(str[i] == letter)
+      {
+        count++;
+      }
+    }
+  }
+  
+  printf("%d", count);
 }
