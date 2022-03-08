e7db9b59412c53efd9ce96b69233a9cbacd6e0ab
BystrovaV <79499241+BystrovaV@users.noreply.github.com>
2022-03-08 14:13:04 +0000

    Add function

--- src/main.c
+++ src/main.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-void main (void)
+int count_letter(char str[], char letter)
 {
-  char letter;
-  fgets(&letter, 2, stdin);
-
-  char buff[2];
-  fgets(buff, 2, stdin);
-
-  char str[1000]="i have to believe that even if  something seems like it cannot be fixed it doesn't mean it's broken ";
-  
   int count = 0;
     
   for(int i=0; i < strlen(str);i++)
@@ -20,6 +12,21 @@ void main (void)
       count++;
     }
   }
+
+  return count;
+}
+
+void main (void)
+{
+  char letter;
+  fgets(&letter, 2, stdin);
+
+  char buff[2];
+  fgets(buff, 2, stdin);
+
+  char str[1000]="i have to believe that even if  something seems like it cannot be fixed it doesn't mean it's broken ";
+
+  int count = count_letter(str, letter);
   
-  printf("%d", count);
+  printf("The letter occurs: %d", count);
 }
